edb6f6bd1b4077e1757a074f894fc7cd71d9545a
Catalin_Andrei <163915311+catal1nBuretea@users.noreply.github.com>
2025-01-16 11:50:54 +0200

    Add files via upload

---
+++ Administrator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp37
+{
+    public class Administrator : Utilizator
+    {
+        private List<Imprimante> imprimante;
+        private List<Comanda> comenzi;
+        private Dictionary<string, double> stocFilament;
+
+        public Administrator(string numeUtilizator, string parola) : base(numeUtilizator, parola)
+        {
+            imprimante = new List<Imprimante>();
+            comenzi = new List<Comanda>();
+            stocFilament = new Dictionary<string, double>();
+        }
+
+        public void AdaugaImprimanta(Imprimante imprimanta)
+        {
+            imprimante.Add(imprimanta);
+            Console.WriteLine($"Imprimanta {imprimanta.ModelImprimanta} adăugată.");
+        }
+
+        public void VizualizeazaImprimantele()
+        {
+            foreach (var a in imprimante)
+            {
+                Console.WriteLine($"Model: {a.ModelImprimanta}, Producător: {a.ProducatorImprimanta}, Cost: {a.CostImprimanta} RON");
+            }
+        }
+
+        public void VizualizeazaDetaliiImprimanta(string model)
+        {
+            var imprimanta = imprimante.FirstOrDefault(i => i.ModelImprimanta == model);
+            if (imprimanta != null)
+            {
+                Console.WriteLine($"Detalii imprimanta {imprimanta.ModelImprimanta}: Producător {imprimanta.ProducatorImprimanta}, Cost: {imprimanta.CostImprimanta}");
+            }
+            else
+            {
+                Console.WriteLine("Imprimanta nu a fost găsită.");
+            }
+        }
+
+        public void VizualizeazaComenzi()
+        {
+            foreach (var comanda in comenzi)
+            {
+                comanda.AfisareDetaliiComanda();
+            }
+        }
+        public void AdaugaComanda(Comanda comanda)
+        {
+            comenzi.Add(comanda);
+            Console.WriteLine($"Comanda pentru {comanda.NumeObiect} a fost adăugată.");
+        }
+        public void ReumplereImprimantaCuRasina(string model, double cantitate)
+        {
+            foreach (var a in imprimante)
+            {
+                if (a is ImprimantaCuRasina imprimantaCuRasina && a.ModelImprimanta == model)
+                {
+                    imprimantaCuRasina.ReumplereRasina(cantitate);
+                    return;
+                }
+            }
+            Console.WriteLine("Imprimanta cu rasina nu a fost gasita.Momentan nu este in stoc ceea ce dumeneavoastra doriti.");
+        }
+        public void AdaugareFilamentInStoc(string Tip, double cantitate)
+        {
+            if (stocFilament.ContainsKey(Tip))
+            {
+                stocFilament[Tip] += cantitate;
+            }
+            else
+            {
+                stocFilament[Tip] = cantitate;
+            }
+            Console.WriteLine($"Au fost adaugati {cantitate} metri de filament tip {Tip}");
+        }
+        public void SchimbaFilament(string model, double cantitate, string culoare)
+        {
+            foreach (var a in imprimante)
+            {
+                if (a is ImprimantaCuFilament imprimantaCuFilament && a.ModelImprimanta == model)
+                {
+                    imprimantaCuFilament.SchimbareFilament(cantitate, culoare);
+                    return;
+                }
+            }
+            Console.WriteLine("Imprimanta cu filament nu a fost gasita.Momentan nu este in stoc ceea ce dumneavoastra doriti.");
+        }
+        public void VizualizeazaFilament()
+        {
+            Console.WriteLine("Stocul de filament:");
+            foreach (var a in stocFilament)
+            {
+                Console.WriteLine($"Tip:{a.Key}, Canitate:{a.Value} metri");
+            }
+        }
+        
+        public void ProcesareComenzi()
+        {
+            Console.WriteLine("Comenzile au fost procesate cu succes!");
+            
+        }
+        public void VizualizeazaToateComenzile()
+        {
+            Console.WriteLine("Lista comenzilor procesate:");
+            foreach (var comanda in comenzi)
+            {
+                comanda.AfisareDetaliiComanda();
+            }
+        
+        }
+    }
+}
---
+++ Comanda.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp37;
+
+public class Comanda
+{
+    public string NumeObiect { get; set; }
+    public double GreutateObiect { get; set; }
+    //Greutatea trebuie sa fie in grame
+    public string CuloareObiect { get; set; }
+    public string AdresaLivrare { get; set; }
+    public double CostFinal { get; set; }
+
+    public Comanda(string numeObiect, double greutateObiect, string culoareObiect, string adresaLivrare,
+        double costFinal)
+    {
+        NumeObiect = numeObiect;
+        GreutateObiect = greutateObiect;
+        CuloareObiect = culoareObiect;
+        AdresaLivrare = adresaLivrare;
+        CostFinal = costFinal;
+    }
+
+    public void AfisareDetaliiComanda()
+    {
+        Console.WriteLine("Comanda plasata:\n");
+        Console.WriteLine($"Obiect: {NumeObiect}\n");
+        Console.WriteLine($"Masa: {GreutateObiect}\n");
+        Console.WriteLine($"Culoare:{CuloareObiect}\n");
+        Console.WriteLine($"Adresa:{AdresaLivrare}\n");
+        Console.WriteLine($"Cost:{CostFinal}\n");
+        
+    }
+    
+}
